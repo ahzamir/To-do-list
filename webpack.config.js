cf4fd7ba325cbd32934a412665c92769f825e0b5
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-05-13 15:34:06 +0430

    add the htmlplugin config for html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
