001dc3a9e3904a2d537bd428ee02e5052754ca14
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-05-14 13:39:10 +0430

    add the add function in DOM content loaded

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import _ from 'lodash';// eslint-disable-line no-unused-vars
 import './style.css';
+import Task from './modules/lists.js';
 
-const arrTasks = [
-  {
-    description: 'forth',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'first',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'thirth',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'second',
-    completed: false,
-    index: 1,
-  },
-];
-const displayTasks = () => {
+const creatNewTasks = (arrTasks) => {
   arrTasks.sort((a, b) => a.index - b.index);
   const tasks = document.getElementById('tasks');
+  tasks.innerHTML = '';
   const everyTasks = document.createElement('div');
   everyTasks.classList.add('tasksss');
   arrTasks.forEach((task) => {
@@ -34,8 +14,28 @@ const displayTasks = () => {
     everyTasks.insertAdjacentHTML('beforeend', everyTask);
   });
   tasks.appendChild(everyTasks);
+  localStorage.setItem('tasks', JSON.stringify(arrTasks));
+};
+const addNewTask = () => {
+  const inputAdd = document.getElementById('inputAdd');
+  tasksObject.add(inputAdd.value);
+  creatNewTasks(arrTasks);
+  inputAdd.value = '';
 };
 
+const tasksObject = new Task(JSON.parse(localStorage.getItem('tasks')));
+const arrTasks = tasksObject.arrTasks;
 window.addEventListener('DOMContentLoaded', () => {
-  displayTasks();
+  const addPart = document.getElementById('addPart');
+  const inputAdd = document.createElement('input');
+  const addButton = document.createElement('i');
+  addButton.classList.add('fa-solid')
+  addButton.classList.add('fa-plus')
+  inputAdd.setAttribute('type', 'text');
+  inputAdd.setAttribute('placeholder', 'Add to your list...');
+  inputAdd.setAttribute('id', 'inputAdd')
+  addPart.appendChild(inputAdd);
+  addPart.appendChild(addButton);
+  creatNewTasks(arrTasks);
+  addButton.addEventListener('click', () => addNewTask());
 });
