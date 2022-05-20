d403ff3a78c3e936025b5fe6c21149a0ffd9152d
freddykaberuka <freddykaberuka2009@gmail.com>
2022-05-20 12:55:55 +0200

    add testing

--- src/to-do-list.test.js
+++ src/to-do-list.test.js
@@ -1,5 +1,4 @@
 import Task from './modules/lists.js';
-// import { setLocalStorage, getLocalStorage } from './modules/localStorage.js';
 
 const tasksObject = new Task();
 describe('Task function', () => {
@@ -8,23 +7,21 @@ describe('Task function', () => {
   tasksObject.add('task 2');
   tasksObject.add('task 3');
   tasksObject.add('task 4');
-  const taskTest = 'Testing task'
 
   test('Add function', () => {
     expect(tasksObject.arrTasks[0].description).toBe('task 0');
     expect(tasksObject.arrTasks.length).toBe(5);
+    expect(tasksObject.length).not.toBe(0);
   });
-  test()
+
   test('Remove function', () => {
     tasksObject.remove(1);
     expect(tasksObject.arrTasks[1].description).toBe('task 2');
+    expect(tasksObject.arrTasks.length).toBe(4);
   });
-
 
   test('Edit function', () => {
     tasksObject.edit(1, 'task 1');
     expect(tasksObject.arrTasks[1].description).toBe('task 1');
-    expect(tasksObject.arrTasks.length).toBe(4);
   });
-
 });
